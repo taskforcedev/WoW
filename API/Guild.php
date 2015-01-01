7a2d362840e37c75b271176ec801ba8502cb0953
Taskforcedev <taskforce2eu@gmail.com>
2015-01-01 14:08:49 +0000

    Add API helper methods.

---
+++ API/Guild.php
@@ -0,0 +1,83 @@
+<?php
+
+class Guild
+{
+    public $api_key;
+    public $region;
+    public $locale;
+
+    /**
+     * Region, locale and key can be defined in constructor or can be set via setters (whichever is preferred).
+     *
+     * @param null $region
+     * @param null $locale
+     * @param null $api_key
+     */
+    public function __construct($region = null, $locale = null, $api_key = null)
+    {
+        if (isset($region)) { $this->region = $region; }
+        if (isset($locale)) { $this->locale = $locale; }
+        if (isset($api_key)) { $this->api_key = $api_key; }
+    }
+
+    /**
+     * Set the locale (language) to request data in.
+     *
+     * @param mixed $locale
+     */
+    public function setLocale($locale)
+    {
+        $this->locale = $locale;
+    }
+
+    /**
+     * Set the region for the API to interact with.
+     *
+     * @param $region
+     */
+    public function setRegion($region)
+    {
+        $this->region = $region;
+    }
+
+    /**
+     * Set your key for the battle.net API.
+     *
+     * @param $api_key
+     */
+    public function setApiKey($api_key)
+    {
+        $this->api_key = $api_key;
+    }
+
+    /**
+     * Helper method for retrieving guild info.
+     *
+     * To use this simply implement your method of requesting the data and parsing the return (we use guzzle but are keeping the git generic).
+     *
+     * @param string $guild The name of the guild to retrieve - this will need to be url encoded.
+     * @param string $server The name of the server on which the character resides. - this may need to be url encoded.
+     * @param array|string $fields The character fields to retrieve, either an array or comma seperated string.
+     *
+     * @return string $url;
+     */
+    public function getGuildApiUrl($guild, $server, $fields = null)
+    {
+        $url = "https://{$this->region}.api.battle.net/wow/guild/{$server}/{$guild}";
+
+        $url .= "?locale=" . $this->locale;
+        $url .= "&api_key=" . $this->api_key;
+
+        if ($fields !== null) {
+            $url .= "&fields=";
+            if (is_array($fields)) {
+                $fields = implode(",", $fields);
+            }
+            $url .= $fields;
+        }
+
+        return $url;
+    }
+
+
+}

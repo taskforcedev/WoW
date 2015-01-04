1052ac9a9a28752e127c90c1fa079ff2eebe3b6e
Taskforcedev <taskforce2eu@gmail.com>
2015-01-04 22:26:51 +0000

    Refactored Api Classes

---
+++ API/ApiRequestable.php
@@ -0,0 +1,52 @@
+<?php
+
+abstract class ApiRequestable
+{
+	protected $api_key;
+    public $region;
+    public $locale;
+
+	/**
+     * Region, locale and key can be defined in constructor or can be set via setters (whichever is preferred).
+     * @param null $region
+     * @param null $locale
+     * @param null $api_key
+     */
+    public function __construct($region = null, $locale = null, $api_key = null)
+    {
+        if (isset($region)) { $this->region = $region; }
+        if (isset($locale)) { $this->locale = $locale; }
+        if (isset($api_key)) { $this->api_key = $api_key; }
+    }
+
+    /**
+     * Set the locale (language) to request data in.
+     *
+     * @param mixed $locale
+     */
+    public function setLocale($locale)
+    {
+        $this->locale = $locale;
+    }
+
+    /**
+     * Set the region for the API to interact with.
+     *
+     * @param $region
+     */
+    public function setRegion($region)
+    {
+        $this->region = $region;
+    }
+
+    /**
+     * Set your key for the battle.net API.
+     *
+     * @param $api_key
+     */
+    public function setApiKey($api_key)
+    {
+        $this->api_key = $api_key;
+    }
+
+}
